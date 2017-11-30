d5dd2b64610a98659c336593684845f6711f6b94
apcsa <apcsa@10.254.1.206>
2017-11-30 13:29:23 -0800

    Lets hope this works

---
+++ src/binnland/christian/Axe.java
@@ -0,0 +1,16 @@
+package binnland.christian;
+
+public class Axe implements Item{
+
+	@Override
+	public String getInfo() {
+		// TODO Auto-generated method stub
+		return this.INFO;
+	}
+	
+	this.Type = Types.WEAPON;
+	INFO = "A shiny metal skull-crusher";
+	NAME = "Axe";
+
+}
+
--- src/binnland/christian/Game.java
+++ src/binnland/christian/Game.java
@@ -3,11 +3,19 @@ package binnland.christian;
 import java.util.List;
 
 public class Game {
-
+	
+	
+	
 	List<Item> items;
 	public static void main(String[] args) {
-		items = new list 
+		println("Welcome to binland");
+		
+		
 
+	}
+	
+	static void println(Object e) {
+		System.out.println(e);
 	}
 
 }
